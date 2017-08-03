9a3d518aa3c11ccb611ab646d59982913af3a466
SureCoin Dev2 <cryptoelastic@users.noreply.github.com>
2017-08-03 23:18:52 +0800

    SureCoin configuration: NETWORK_ID
     * Changed NETWORK_ID (generated new array of random bytes)

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -149,7 +149,11 @@ namespace config
   uint16_t const P2P_DEFAULT_PORT = 18080;
   uint16_t const RPC_DEFAULT_PORT = 18081;
   boost::uuids::uuid const NETWORK_ID = { {
+#if 0   // Monero original
       0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
+#else      
+      0x62, 0x7c, 0x22, 0x42, 0xa3, 0x39, 0xfc, 0x9d, 0x1f, 0x9c, 0x6d, 0xcc, 0xf4, 0x24, 0x70, 0x54
+#endif      
     } }; // Bender's nightmare
   std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
   uint32_t const GENESIS_NONCE = 10000;
